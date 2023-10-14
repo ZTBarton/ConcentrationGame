0f9e8424aa22acd7e537316b1603d1ff7d70db6d
Zach Barton <zachary.barton5@gmail.com>
2023-10-14 16:56:39 -0600

    Initial Commit

---
+++ Concentration Game/Concentration_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concentration_GameApp.swift
+//  Concentration Game
+//
+//  Created by Zach Barton on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Concentration_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
